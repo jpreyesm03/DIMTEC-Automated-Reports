5d724b76d94e43adb497865996f02dbb20d11c69
Juan Pablo Reyes <jpreyesm03@gmail.com>
2024-08-15 00:28:14 -0600

    Check Simplenotes for my roadmap!

--- Week 4/graphics_generator.py
+++ Week 4/graphics_generator.py
@@ -1,2 +1,1 @@
-print("Push shortcut papi, JP branch")
-print("I starte the Odin Project today!")
+print("I often fail to realize that there isn't only one path. What is true is that I am learning a lot, ant this is what it's important")
